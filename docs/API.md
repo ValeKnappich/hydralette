6aca43396c53c27d2a837a4c6ebac45065baad02
ValeKnappich <valentin.knappich@web.de>
2024-01-29 12:48:08 +0100

    version 0.2.0

--- docs/API.md
+++ docs/API.md
@@ -1,6 +1,23 @@
 # API Reference
 
-::: hydralette.ConfigBase
+
+::: hydralette.Config
+    options:
+        show_root_heading: true
+        show_if_no_docstring: true
+        show_signature: true
+        heading_level: 3
+        members: [__init__, apply, override, check_required_args, validate, resolve_references, print_help, from_dict, to_dict]
+
+::: hydralette.Field
+    options:
+        show_root_heading: true
+        show_if_no_docstring: true
+        show_signature: true
+        heading_level: 3
+        members: [__init__, value]
+
+<!-- ::: hydralette.ConfigBase
     options:
         show_root_heading: true
         show_if_no_docstring: true
@@ -52,4 +69,4 @@
     options:
         show_root_heading: true
         show_if_no_docstring: true
-        heading_level: 3
+        heading_level: 3 -->
--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,33 +1,74 @@
+import json
 from pathlib import Path
 
 import pytest
 
-from hydralette import MISSING, ConfigBase, HydraletteConfigurationError, field
+from hydralette import Config, Field, ValidationError
 
 
-class ModelConfig(ConfigBase):
-    n_layers: int = field(default=32, metadata=dict(help="Number of Layers"))
-    dropout: float = field(default=MISSING, metadata=dict(help="Dropout p value"))  # this is a required argument
+def test_1():
+    cfg = Config(a=1, b=2, c="abc")
+    assert cfg.to_dict() == {"a": 1, "b": 2, "c": "abc"}
 
 
-class Config(ConfigBase):
-    output_dir: Path = field(default=Path("output"), metadata=dict(help="Output directory to save all results to"))
-    model: ModelConfig = field(default_factory=lambda: ModelConfig(), metadata=dict(help="Config for model"))
+def test_2():
+    cfg = Config(a=Field(default=1, help="Lorem ipsum"), b=Config(c=Field(default=2, help="Lorem ipsum")))
+    cfg.apply(["--b.c", "4"])
+    assert cfg.b.c == 4
 
 
-def test_cli_overrides():
-    cli_args = "model.dropout=1e-5 output_dir=my_output_directory".split(" ")
-    config = Config.create(cli_args)
-    assert config.to_dict() == {"output_dir": Path("my_output_directory"), "model": {"n_layers": 32, "dropout": 1e-5}}
+def test_3():
+    cfg = Config(
+        model=Config(
+            _groups={
+                "_default": "rnn",
+                "rnn": Config(n_layers=2, bidirectional=False),
+                "transformer": Config(n_layers=16, num_attention_heads=8),
+            }
+        )
+    )
+    cfg.apply(["--model", "transformer"])
+    assert cfg.to_dict() == {"model": {"n_layers": 16, "num_attention_heads": 8}}
 
 
-def test_yaml_overrides():
-    yaml_overrides = {"model": {"dropout": 1e-5}, "output_dir": "my_output_directory"}
-    config = Config.create(yaml_overrides=yaml_overrides)
-    assert config.to_dict() == {"output_dir": Path("my_output_directory"), "model": {"n_layers": 32, "dropout": 1e-5}}
+def test_4():
+    def calc(a: int, b=2):
+        pass
+
+    cfg = Config(_from_signature=calc)
+    cfg.apply(["--a", "42"])
+    assert cfg.to_dict() == {"a": 42, "b": 2}
 
 
-def test_missing_required_arg():
-    with pytest.raises(HydraletteConfigurationError):
-        cli_args = "output_dir=my_output_directory".split(" ")
-        config = Config.create(cli_args)  # noqa: F841
+def test_5():
+    cfg = Config(my_dict=Field(default={"a": 1, "b": {"c": 2}}, convert=json.loads))
+    cfg.apply(["--my_dict", r'{"a": 2, "b": {"c": 3}}'])
+    assert cfg.my_dict == {"a": 2, "b": {"c": 3}}
+
+
+def test_6():
+    cfg = Config(n=Field(default=1, validate=lambda n: n > 0))
+    with pytest.raises(ValidationError):
+        cfg.apply(["--n", "-1"])
+        # throws: ValidationError: Field validation failed for -1
+
+    cfg = Config(_validate=lambda cfg: cfg.a > cfg.b, a=1, b=2)
+    with pytest.raises(ValidationError):
+        cfg.apply()  # or cfg.validate()
+        # throws: ValidationError: Config validation failed for {'a': 1, 'b': 2}
+
+
+def test_7():
+    cfg = Config(
+        dir=Path("outputs"),
+        train=Config(
+            checkpoint_dir=Field(reference_root=lambda cfg: cfg.dir / "checkpoints"),  # relative to current config
+            metrics_dir=Field(reference=lambda cfg: cfg.checkpoint_dir.parent / "metrics"),  # relative to root config
+        ),
+    )
+    cfg.resolve_references()
+
+    assert cfg.to_dict() == {
+        "dir": Path("outputs"),
+        "train": {"checkpoint_dir": Path("outputs/checkpoints"), "metrics_dir": Path("outputs/metrics")},
+    }
